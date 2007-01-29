48ce8c64044dd694440505f996857f9be532087b
Dafydd Harries <dafydd.harries@collabora.co.uk>
2007-01-29 15:10:00 +0000

    split out candidate code
    darcs-hash:20070129151050-c9803-eac37135f61f529b6fcf0db513db10beeb3102ae.gz

--- agent/agent.h
+++ agent/agent.h
@@ -8,47 +8,7 @@
 
 #include "udp.h"
 #include "address.h"
-
-
-/*** candidate ***/
-
-typedef enum candidate_type CandidateType;
-
-enum candidate_type
-{
-  CANDIDATE_TYPE_HOST,
-  CANDIDATE_TYPE_SERVER_REFLEXIVE,
-  CANDIDATE_TYPE_PEER_REFLEXIVE,
-  CANDIDATE_TYPE_RELAYED,
-};
-
-
-typedef struct _candidate Candidate;
-
-struct _candidate
-{
-  CandidateType type;
-  guint id;
-  Address addr;
-  Address base_addr;
-  guint16 port;
-  guint32 priority;
-  guint stream_id;
-  guint component_id;
-  // guint generation;
-  // gchar *foundation;
-  UDPSocket sock;
-};
-
-
-Candidate *
-candidate_new (CandidateType type);
-void
-candidate_free (Candidate *candidate);
-gfloat
-candidate_jingle_priority (Candidate *candidate);
-guint32
-candidate_ice_priority (Candidate *candidate);
+#include "candidate.h"
 
 
 /*** event ***/
