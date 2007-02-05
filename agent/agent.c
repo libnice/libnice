c0f689a4eb9a55d03baad079c0310bb5513c3c00
Dafydd Harries <dafydd.harries@collabora.co.uk>
2007-02-05 12:39:00 +0000

    add blank line
    darcs-hash:20070205123907-c9803-f0503bc94290639183ee703ceef77ac592807b8b.gz

--- agent/agent.c
+++ agent/agent.c
@@ -218,6 +218,7 @@ nice_agent_add_local_host_candidate (
   sin.sin_family = AF_INET;
   sin.sin_addr.s_addr = htonl (address->addr_ipv4);
   sin.sin_port = 0;
+
   /* XXX: handle error */
   if (!nice_udp_socket_factory_make (agent->socket_factory,
         &(candidate->sock), &sin))
