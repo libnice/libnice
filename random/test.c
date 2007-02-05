be9f196d83732177068cdebbf063e4325a794f94
Dafydd Harries <dafydd.harries@collabora.co.uk>
2007-02-05 19:11:00 +0000

    test nice_rng_generate_bytes
    darcs-hash:20070205191117-c9803-4b3ccb476da684d25f11ab287eb4c0842be61f1b.gz

--- random/test.c
+++ random/test.c
@@ -9,12 +9,18 @@ main (void)
   NiceRNG *rng;
   gchar buf[9];
 
+  buf[8] = '\0';
+
   nice_rng_set_new_func (nice_rng_glib_new_predictable);
   rng = nice_rng_new ();
+
   nice_rng_generate_bytes_print (rng, 8, buf);
-  buf[8] = '\0';
   //g_debug ("%s", buf);
   g_assert (0 == strcmp (buf, "S9PObXR5"));
+
+  nice_rng_generate_bytes (rng, 8, buf);
+  g_assert (0 == strcmp (buf, "\x09\xd3\x15\xf2\x24\x57\x46\xd8"));
+
   nice_rng_free (rng);
   return 0;
 }
