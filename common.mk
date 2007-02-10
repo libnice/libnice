5e2b888e36167259959ab59401ecc4b35eb5f527
Dafydd Harries <dafydd.harries@collabora.co.uk>
2007-02-10 13:29:00 +0000

    define ERROR_CFLAGS with -W* in common.mk
    darcs-hash:20070210132929-c9803-92624737ec43cff874b758d687b1821005882b04.gz

--- common.mk
+++ common.mk
@@ -1,4 +1,13 @@
 
+ERROR_CFLAGS = \
+	-Wall \
+	-Werror \
+	-Wextra \
+	-Wno-missing-field-initializers \
+	-Wwrite-strings \
+	-Wmissing-prototypes \
+	-Wredundant-decls
+
 check-valgrind:
 	$(MAKE) TESTS_ENVIRONMENT="sh $(abspath $(top_srcdir))/scripts/valgrind.sh" check
 
