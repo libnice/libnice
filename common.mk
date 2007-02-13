f957f9932ccb21583df0932b72b98b0b33711712
Dafydd Harries <dafydd.harries@collabora.co.uk>
2007-02-13 14:29:00 +0000

    common.mk: include .gcno and .gcda files in CLEANFILES
    darcs-hash:20070213142907-c9803-39073001a58cede566543b4d56cbdbfc614cb9a7.gz

--- common.mk
+++ common.mk
@@ -8,6 +8,8 @@ ERROR_CFLAGS = \
 	-Wmissing-prototypes \
 	-Wredundant-decls
 
+CLEANFILES = *.gcno *.gcda
+
 check-valgrind:
 	$(MAKE) TESTS_ENVIRONMENT="sh $(abspath $(top_srcdir))/scripts/valgrind.sh" check
 
