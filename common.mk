cde5ed64438486e56af40562e8740be1b479a137
Kai Vehmanen <first.surname@nokia.com>
2007-05-02 14:23:00 +0000

    Enabled more compiler warnings
    darcs-hash:20070502142335-77cd4-d6daeb49f463440ca7bfec752382a982913d41ae.gz

--- common.mk
+++ common.mk
@@ -3,14 +3,23 @@ ERROR_CFLAGS = \
 	-Wall \
 	-Werror \
 	-Wextra \
+	-Wundef \
+	-Wnested-externs \
+	-Wvolatile-register-var \
 	-Wno-missing-field-initializers \
 	-Wwrite-strings \
+	-Wpointer-arith \
+	-Wbad-function-cast \
+	-Wmissing-declarations \
 	-Wmissing-prototypes \
+	-Wstrict-prototypes \
 	-Wredundant-decls \
 	-Wno-unused-parameter
+# -Wold-style-definition -Winline -Wunreachable-code
 
 CLEANFILES = *.gcno *.gcda
 
 check-valgrind:
 	$(MAKE) TESTS_ENVIRONMENT="sh $(abspath $(top_srcdir))/scripts/valgrind.sh" check
 
+.PHONY: check-valgrind
