a1db26c45daa87eb07cd36c064d12086330f36c6
Kai Vehmanen <first.surname@nokia.com>
2007-05-21 15:24:00 +0000

    Build system updates: modified lcov rules.
    darcs-hash:20070521152408-77cd4-619e4676d30ff65fb56785f1152b49635e238ea9.gz

--- nice/test-symbols.sh
+++ nice/test-symbols.sh
@@ -10,6 +10,12 @@ check_symbols=$srcdir/../scripts/check-symbols.sh
 
 if ! test -f $check_symbols; then
 	echo "can't find check-symbols.sh"
+	exit 1
+fi
+
+if ! test -f .libs/libnice.so; then
+	echo "not building shared object" >&2
+	exit 77
 fi
 
 sh $check_symbols .libs/libnice.so $srcdir/libnice.symbols
