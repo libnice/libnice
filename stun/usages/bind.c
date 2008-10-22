ec7dbae5b7346810421bbceb27383167bce7fe53
Youness Alaoui <kakaroto@kakaroto.(none)>
2008-10-22 18:26:06 -0400

    stunbdc and stund should use 3489bis to allow the unit tests to run

--- stun/usages/bind.c
+++ stun/usages/bind.c
@@ -170,7 +170,7 @@ StunUsageBindReturn stun_usage_bind_run (const struct sockaddr *srv,
   StunUsageBindReturn bind_ret;
 
   stun_agent_init (&agent, STUN_ALL_KNOWN_ATTRIBUTES,
-      STUN_COMPATIBILITY_RFC3489, 0);
+      STUN_COMPATIBILITY_3489BIS, STUN_AGENT_USAGE_USE_FINGERPRINT);
 
   len = stun_usage_bind_create (&agent, &req, req_buf, sizeof(req_buf));
 
