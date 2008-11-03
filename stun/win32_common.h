c1f4fcb3eea650cb1ccc542612d3de09d967920a
Youness Alaoui <kakaroto@kakaroto.(none)>
2008-11-03 16:35:06 -0500

    add stdint.h and stdbool.h replacement for windows

---
+++ stun/win32_common.h
@@ -0,0 +1,41 @@
+/* ISO C9x  7.18  Integer types <stdint.h>
+ * Based on ISO/IEC SC22/WG14 9899 Committee draft (SC22 N2794)
+ *
+ *  THIS SOFTWARE IS NOT COPYRIGHTED
+ *
+ *  Contributor: Danny Smith <danny_r_smith_2001@yahoo.co.nz>
+ *
+ *  This source code is offered for use in the public domain. You may
+ *  use, modify or distribute it freely.
+ *
+ *  This code is distributed in the hope that it will be useful but
+ *  WITHOUT ANY WARRANTY. ALL WARRANTIES, EXPRESS OR IMPLIED ARE HEREBY
+ *  DISCLAIMED. This includes but is not limited to warranties of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  Date: 2000-12-02
+ */
+
+
+#ifndef _WIN32_COMMON_H
+#define _WIN32_COMMON_H
+
+
+/* 7.18.1.1  Exact-width integer types */
+typedef signed char int8_t;
+typedef unsigned char   uint8_t;
+typedef short  int16_t;
+typedef unsigned short  uint16_t;
+typedef int  int32_t;
+typedef unsigned   uint32_t;
+typedef long long  int64_t;
+typedef unsigned long long   uint64_t;
+typedef unsigned int size_t;
+typedef unsigned long ssize_t;
+
+typedef uint8_t bool;
+#define true 1
+#define false 0
+
+
+#endif /* _WIN32_COMMON_H */
