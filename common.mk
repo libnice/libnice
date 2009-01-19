c4c2977f7a09faa40ee79ae26d6c9f8cb404b15b
Youness Alaoui <youness.alaoui@collabora.co.uk>
2009-01-19 00:00:18 -0500

    Fix include files getting installed to /usr/include/libnice after the name change... it should be kept as /usr/include/nice to not break the API

--- common.mk
+++ common.mk
@@ -16,6 +16,9 @@ ERROR_CFLAGS = \
 
 CLEANFILES = *.gcno *.gcda
 
+pkgincludedir = $(includedir)/nice
+
+
 check-valgrind:
 	$(MAKE) TESTS_ENVIRONMENT="sh $$(cd "$(top_srcdir)" && pwd)/scripts/valgrind.sh" check
 
