60108eae9827266ece0b66a838ed99e7c7ac83cd
Marcus Lundblad <ml@update.uu.se>
2010-09-30 22:19:49 +0200

    These line-breaks where not nessesary...

--- socket/turn.c
+++ socket/turn.c
@@ -91,10 +91,8 @@ typedef struct {
   bool ms_connection_id_valid;
   GHashTable *permissions;		/* stores installed permissions */
   GHashTable *sent_permissions; /* ongoing permission installed */
-  GHashTable *send_data_queues; /* stores a send data queue for per
-	 								peer */
-  guint permission_timeout_source;	/* timer used to invalidate
-	 									permissions */
+  GHashTable *send_data_queues; /* stores a send data queue for per peer */
+  guint permission_timeout_source;	/* timer used to invalidate permissions */
   gboolean has_binding;
   gboolean sent_binding;
   guint binding_timeout_source;
