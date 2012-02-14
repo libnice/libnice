db6f5f33aee8f136192e2833a17b7a9caa115ff1
Rohan Garg <rohangarg@kubuntu.org>
2012-02-14 22:54:17 +0530

    Use size of the MD5Context, not size of the pointer

--- stun/md5.c
+++ stun/md5.c
@@ -162,7 +162,7 @@ void MD5Final(unsigned char digest[16], struct MD5Context *ctx)
   MD5Transform(ctx->buf, (uint32_t *) ctx->in);
   byteReverse((unsigned char *) ctx->buf, 4);
   memcpy(digest, ctx->buf, 16);
-  memset(ctx, 0, sizeof(ctx));	/* In case it's sensitive */
+  memset(ctx, 0, sizeof(struct MD5Context));	/* In case it's sensitive */
 }
 
 /* The four core functions - F1 is optimized somewhat */
