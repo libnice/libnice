14854e76470aa8022df8a5a070661cf4f15010a9
Colin Walters <walters@verbum.org>
2013-09-04 09:12:33 -0400

    autogen.sh: Honor NOCONFIGURE=1
    This is adapted from telepathy-logger's autogen.sh.  See
    http://people.gnome.org/~walters/docs/build-api.txt
    for rationale behind NOCONFIGURE=1.

--- autogen.sh
+++ autogen.sh
@@ -6,16 +6,21 @@ gtkdocize || exit 1
 
 autoreconf -fi
 
-run_configure=true
-for arg in $*; do
-    case $arg in
-        --no-configure)
-            run_configure=false
-            ;;
-        *)
-            ;;
-    esac
-done
+# Honor NOCONFIGURE for compatibility with gnome-autogen.sh
+if test x"$NOCONFIGURE" = x; then
+    run_configure=true
+    for arg in $*; do
+        case $arg in
+            --no-configure)
+                run_configure=false
+                ;;
+            *)
+                ;;
+        esac
+    done
+else
+    run_configure=false
+fi
 
 if test $run_configure = true; then
     ./configure "$@"
