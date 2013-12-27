574b59a3550238d5a329903b5a03f817ae8bbd45
Olivier Crête <olivier.crete@collabora.com>
2013-12-26 21:16:14 -0500

    configure: Verify that the compiler understands warning cflags
    Older compilers don't understand all of the flags

--- common.mk
+++ common.mk
@@ -1,21 +1,3 @@
-
-ERROR_CFLAGS = \
-	$(LIBNICE_CFLAGS) \
-	-fno-strict-aliasing \
-	-Wextra \
-	-Wundef \
-	-Wnested-externs \
-	-Wwrite-strings \
-	-Wpointer-arith \
-	-Wbad-function-cast \
-	-Wmissing-declarations \
-	-Wmissing-prototypes \
-	-Wstrict-prototypes \
-	-Wredundant-decls \
-	-Wno-unused-parameter \
-	-Wno-missing-field-initializers
-# -Wold-style-definition -Winline -Wunreachable-code
-
 CLEANFILES = *.gcno *.gcda
 
 pkgincludedir = $(includedir)/nice
