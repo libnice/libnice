8da016a76c9b05c56fe00d4cd1cd26813cf69951
Olivier Crête <olivier.crete@collabora.com>
2014-04-28 23:10:54 -0400

    threaded-example: Make sure the foundation string is NULL terminated

--- examples/threaded-example.c
+++ examples/threaded-example.c
@@ -331,6 +331,7 @@ parse_candidate(char *scand, guint stream_id)
   cand->stream_id = stream_id;
   cand->transport = NICE_CANDIDATE_TRANSPORT_UDP;
   strncpy(cand->foundation, tokens[0], NICE_CANDIDATE_MAX_FOUNDATION);
+  cand->foundation[NICE_CANDIDATE_MAX_FOUNDATION - 1] = 0;
   cand->priority = atoi (tokens[1]);
 
   if (!nice_address_set_from_string(&cand->addr, tokens[2])) {
