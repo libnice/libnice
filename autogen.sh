1493a381d5bf6e15348c2bc17270f45b69cb70d2
Philip Withnall <philip@tecnocode.co.uk>
2016-03-01 23:50:11 +0000

    build: Update autogen.sh from GNOME template
    https://wiki.gnome.org/Projects/GnomeCommon/Migration#autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,27 +1,38 @@
 #!/bin/sh
-set -e
+# Run this to generate all the initial makefiles, etc.
+test -n "$srcdir" || srcdir=$(dirname "$0")
+test -n "$srcdir" || srcdir=.
 
-test -d m4 || mkdir m4
-gtkdocize || exit 1
+olddir=$(pwd)
 
-autoreconf -fi
+cd $srcdir
 
-# Honor NOCONFIGURE for compatibility with gnome-autogen.sh
-if test x"$NOCONFIGURE" = x; then
-    run_configure=true
-    for arg in $*; do
-        case $arg in
-            --no-configure)
-                run_configure=false
-                ;;
-            *)
-                ;;
-        esac
-    done
-else
-    run_configure=false
+(test -f configure.ac) || {
+	echo "*** ERROR: Directory '$srcdir' does not look like the top-level project directory ***"
+	exit 1
+}
+
+# shellcheck disable=SC2016
+PKG_NAME=$(autoconf --trace 'AC_INIT:$1' configure.ac)
+
+if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
+	echo "*** WARNING: I am going to run 'configure' with no arguments." >&2
+	echo "*** If you wish to pass any to it, please specify them on the" >&2
+	echo "*** '$0' command line." >&2
+	echo "" >&2
 fi
 
-if test $run_configure = true; then
-    ./configure "$@"
+aclocal --install || exit 1
+gtkdocize --copy || exit 1
+autoreconf --verbose --force --install || exit 1
+
+cd "$olddir"
+if [ "$NOCONFIGURE" = "" ]; then
+	$srcdir/configure "$@" || exit 1
+
+	if [ "$1" = "--help" ]; then exit 0 else
+		echo "Now type 'make' to compile $PKG_NAME" || exit 1
+	fi
+else
+	echo "Skipping configure process."
 fi
