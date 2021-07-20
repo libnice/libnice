libnice/libnice

commit b20ac5ba7b731f2cc698f26e8def28a518de058e

agent: transition to state gathering when restarting ice
The state of the component is downgraded to gathering state, so it can
really restart a new conncheck from scratch. It'll transition to state
connecting when receiving the updated remote candidates.

agent/agent.c
@@ -2599,7 +2599,10 @@ void agent_signal_component_state_change (NiceAgent *agent, guint stream_id, gui
             TRANSITION (DISCONNECTED, CONNECTING) ||
             /* If a tcp socket of connected pair is disconnected, in
              * conn_check_prune_socket(): */
-            TRANSITION (CONNECTED, CONNECTING));
+            TRANSITION (CONNECTED, CONNECTING) ||
+            /* with ICE restart in nice_stream_restart() */
+            TRANSITION (CONNECTED, GATHERING) ||
+            TRANSITION (READY, GATHERING));
 
 #undef TRANSITION
 
@@ -5562,8 +5565,8 @@ nice_agent_restart_stream (
     goto done;
   }
 
-  /* step: reset local credentials for the stream and
-   * clean up the list of remote candidates */
+  /* step: reset local credentials for the stream,
+   * clean up the list of candidates, and the conncheck list */
   nice_stream_restart (stream, agent);
 
   res = TRUE;

agent/stream.c
@@ -139,6 +139,8 @@ nice_stream_restart (NiceStream *stream, NiceAgent *agent)
     NiceComponent *component = i->data;
 
     nice_component_restart (component);
+    agent_signal_component_state_change (agent,
+        stream->id, component->id, NICE_COMPONENT_STATE_GATHERING);
   }
 }
 

commit 52268e7e8910d7432a3e9601a467528738936374

agent: Accept any transition back to the gathering state
When doing a ICE restart, which can be done from any state, we can
always go back into the gathering state.

This fixes a regression introduced by b20ac5ba7b731f2cc698f26e8def28a518de058e

agent/agent.c
@@ -2596,9 +2596,9 @@ void agent_signal_component_state_change (NiceAgent *agent, guint stream_id, gui
             /* If a tcp socket of connected pair is disconnected, in
              * conn_check_prune_socket(): */
             TRANSITION (CONNECTED, CONNECTING) ||
-            /* with ICE restart in nice_stream_restart() */
-            TRANSITION (CONNECTED, GATHERING) ||
-            TRANSITION (READY, GATHERING));
+            /* with ICE restart in nice_stream_restart(),
+             * it can always go back to gathering */
+            (new_state == NICE_COMPONENT_STATE_GATHERING));
 
 #undef TRANSITION
 