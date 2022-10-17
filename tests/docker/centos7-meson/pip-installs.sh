8c254c8fa8f8b567b686c68ac1880346d9a8ab18
Olivier Crête <olivier.crete@collabora.com>
2022-10-17 13:48:13 +0200

    ci: Expose full code coverage report to GitLab

--- tests/docker/centos7-meson/pip-installs.sh
+++ tests/docker/centos7-meson/pip-installs.sh
@@ -6,6 +6,7 @@ yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarc
 yum -y install rh-python36 lcov
 
 scl enable rh-python36 "pip3 install meson==0.60.3"
+scl enable rh-python36 "pip3 install lcov-cobertura"
 scl enable rh-python36 "pip3 install ninja"
 
 yum clean all
